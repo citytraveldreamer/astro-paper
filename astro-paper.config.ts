0b35d70b323d7c7a4800a017abb434f271e3bed4
citytraveldreamer <citytraveldreamer@gmail.com>
2026-08-10 01:41:37 +0800

    Update astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,24 +2,24 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://www.citytraveldreamer.com/",
+    url: "https://new.citytraveldreamer.com/",
     title: "都市遊魂",
     description: "躲在相機背後的安靜觀察者，用鏡頭記錄不擁擠的都會質感、品味角落與慢活旅居足跡。",
     author: "都市遊魂",
     profile: "https://www.citytraveldreamer.com/",
     ogImage: "default-og.jpg",
-    lang: "zh-TW",
+    lang: "zh-HK",
     timezone: "Asia/Hong_Kong",
     dir: "ltr",
   },
   posts: {
     perPage: 6,
-    perIndex: 4,
+    perIndex: 6,
     scheduledPostMargin: 15 * 60 * 1000,
   },
   features: {
     lightAndDarkMode: true,
-    dynamicOgImage: false,
+    dynamicOgImage: true,
     showArchives: true,
     showBackButton: true,
     editPost: {
@@ -28,7 +28,10 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "mail", url: "mailto:yourmail@gmail.com" },
+    { name: "mail", url: "mailto:citytraveldreamer@gmail.com" },
+    { name: "facebook", url: "https://www.facebook.com/citytraveldreamer" },
+    { name: "instagram", url: "https://www.instagram.com/citytraveldreamer" },
+    { name: "youtube", url: "https://www.youtube.com/@citytraveldreamer" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
