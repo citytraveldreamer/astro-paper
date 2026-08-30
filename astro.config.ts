e9f0f16d5a54750a08ea87c089093e7b073d7c90
citytraveldreamer <citytraveldreamer@gmail.com>
2026-08-30 15:16:03 +0800

    affiliate link update

--- astro.config.ts
+++ astro.config.ts
@@ -32,6 +32,10 @@ const cloudflareRedirects = () => ({
         "# 🔗 Affiliate Short Links (集中管理區)",
         "# ==========================================",
         
+        "# --- Klook ---",
+        "/go/klook-esim-hk  https://affiliate.klook.com/redirect?aid=383&aff_adid=1407455&k_site=https%3A%2F%2Fwww.klook.com%2Fzh-HK%2Factivity%2F132216-esim-hong-kong-with-high-speed-and-stable-internet-connection%2F  302",
+        "/go/klook-tram  https://affiliate.klook.com/redirect?aid=383&aff_adid=1377806&k_site=https%3A%2F%2Fwww.klook.com%2Fzh-HK%2Factivity%2F765-peak-tram-sky-terrace-hongkong%2F  302",
+
         "# --- GetYourGuide ---",
         "/go/peak-tram-gyg  https://www.getyourguide.com/hong-kong-l174/hongkongpeak-tram-sky-terrace-428-t1340994/?partner_id=13UDXJC&utm_medium=online_publisher&cmp=blog-the-peak-en  302",
         
@@ -44,6 +48,9 @@ const cloudflareRedirects = () => ({
         "/go/airalo-hk  https://tp.media/r?campaign_id=541&marker=219253&p=8310&sub_id=blog-en&trs=283003&u=https%3A%2F%2Fwww.airalo.com%2Fhong-kong-esim  302",
         "# --- Airalo eSIM (YouTube) ---",
         "/yt/airalo-hk  https://tp.media/r?campaign_id=541&marker=219253&p=8310&sub_id=youtube&trs=283003&u=https%3A%2F%2Fwww.airalo.com%2Fhong-kong-esim  302",
+
+        "# --- Agoda: The Murray ---",
+        "/go/murray-hotel-agoda  https://www.agoda.com/partners/partnersearch.aspx?pcs=1&cid=1812007&hl=zh-hk&hid=2728001&ck1=the_peak_post&ck2=editor_tip  302",
 
         "# --- Booking.com: The Murray ---",
         "/go/murray-hotel-en  https://tp.media/r?campaign_id=84&marker=219253&p=2076&sub_id=blog-en&trs=283003&u=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fhk%2Fthe-murray-hong-kong-a-niccolo.html  302",
